2d351828eeef6297e448a80a2a282801e3fa5fd7
FELIPE <felipe3133770868@gmail.com>
2024-04-05 15:20:34 -0500

    Pruebas para clase Persona y Calculadora

--- unittest/funciones.py
+++ unittest/funciones.py
@@ -8,4 +8,29 @@ def factorial(n: int) -> int:
     if n == 0:
         return 1
     return n*factorial(n-1)
+
+def add_list(lista: list, elem: any) -> None:
+    lista.append(elem)
+
+class Persona:
+    def __init__(self: object, nombre: str) -> None:
+        self._nombre = nombre
     
+    def get_nombre(self: object) -> str:
+        return self._nombre
+    
+    def set_nombre(self: object, nuevo_nombre: str) -> None:
+        self._nombre = nuevo_nombre
+
+class Calculadora:
+    def __init__(self) -> None:
+        self._resultado = 0
+    
+    def sumar(self, num1, num2):
+        self._resultado = num1 + num2
+    
+    def restar(self, num1, num2):
+        self._resultado = num1 - num2
+    
+    def get_resultado(self):
+        return self._resultado
--- unittest/test_unittest_1.py
+++ unittest/test_unittest_1.py
@@ -18,6 +18,45 @@ class TestFunciones(unittest.TestCase):
         self.assertEqual(factorial(3), 6)
         self.assertEqual(factorial(4), 24)
         self.assertEqual(factorial(5), 120)
+    
+    def test_add_list(self):
+        lista = []
+        add_list(lista, 5)
+        add_list(lista, 2)
+        add_list(lista, "dfksadk")
+        add_list(lista, [15, 60, 29])
+        add_list(lista, 50)
+        
+        self.assertEqual(lista[-1], 50)
+        self.assertEqual(len(lista), 5)
+
+class TestPersona(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.persona = Persona("Juan")
+    
+    @classmethod
+    def tearDownClass(cls) -> None:
+        del cls.persona
+    
+    def test_get_nombre(self):
+        self.assertEqual(self.persona.get_nombre(), "Juan")
+    
+    def test_set_nombre(self):
+        self.persona.set_nombre("Pedro")
+        self.assertEqual(self.persona.get_nombre(), "Pedro")
+
+class TestCalculadora(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculadora = Calculadora()
+    
+    def test_sumar(self):
+        self.calculadora.sumar(5, 8)
+        self.assertEqual(self.calculadora.get_resultado(), 13)
+    
+    def test_restar(self):
+        self.calculadora.restar(10, 2)
+        self.assertEqual(self.calculadora.get_resultado(), 8)
 
 if __name__ == '__main__':
     unittest.main()
